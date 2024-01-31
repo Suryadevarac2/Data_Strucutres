08b88f7530af96bde1c63ac0d685ed46802fd6fb
Suryadevarac2 <144551520+Suryadevarac2@users.noreply.github.com>
2024-01-30 22:18:22 -0500

    stacks in c++.cpp

--- stacks in c++.cpp
+++ stacks in c++.cpp
@@ -1,20 +1,31 @@
 #include <iostream> 
 #include <stack>
 using namespace std;
-int main() {
-	stack<int> stack;
-	stack.push(21);// The values pushed in the stack should be of the same data which is written during declaration of stack
-	stack.push(22);
-	stack.push(24);
-	stack.push(25);
-	int num = 0;
-	stack.push(num);
-	stack.pop();
-	stack.pop();
-	stack.pop();
 
+int main() {
+    // Creating a stack to hold integers
+	stack<int> stack;
+    
+    // Pushing integers onto the stack
+	stack.push(21); // Pushing 21 onto the stack
+	stack.push(22); // Pushing 22 onto the stack
+	stack.push(24); // Pushing 24 onto the stack
+	stack.push(25); // Pushing 25 onto the stack
+    
+    // Declaring and initializing an integer variable
+	int num = 0;
+    
+    // Pushing the value of 'num' onto the stack
+	stack.push(num); // Pushing the value of 'num' (which is 0) onto the stack
+    
+    // Removing the top element from the stack twice
+	stack.pop(); // Removing the top element from the stack
+	stack.pop(); // Removing the top element from the stack
+	stack.pop(); // Removing the top element from the stack
+
+    // Printing the remaining elements of the stack
 	while (!stack.empty()) {
-		cout << stack.top() << " ";
-		stack.pop();
+		cout << stack.top() << " "; // Printing the top element of the stack
+		stack.pop(); // Removing the top element from the stack
 	}
 }
